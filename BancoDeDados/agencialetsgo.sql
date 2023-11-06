f778074e96cc313fee5831fe76da5b03df07a48f
Alessandra de Luca Rodrigues <133799132+aledelucarodrigues@users.noreply.github.com>
2023-11-06 09:13:00 -0300

    Banco de Dados

---
+++ BancoDeDados/agencialetsgo.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `agencialetsgo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `agencialetsgo`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: agencialetsgo
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `nomeCliente` varchar(255) NOT NULL,
+  `cpfCliente` varchar(11) NOT NULL,
+  `telefoneCliente` varchar(15) NOT NULL,
+  `emailCliente` varchar(255) NOT NULL,
+  `senhaCliente` varchar(20) NOT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `cpfCliente` (`cpfCliente`),
+  UNIQUE KEY `emailCliente` (`emailCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'ALESSANDRA CUNHA DE LUCA RODRIGUES','04444444444','41988073807','alessandra@gmail.com','123456'),(2,'ANALIZE ROSALI CUNHA DE LUCA','03903903939','41988476830','analize@hotmail.com','123456789'),(3,'ANA PAULA DE LUCA','05605605656','41988885555','anapaula@gmail.com','123456'),(5,'RAFAEL BONETTO RODRIGUES','05205203656','41988595959','rafael@gmail.com','123456'),(6,'ISABELA DE LUCA RODRIGUES','08282582582','41988545454','isabela@gmail.com','123456'),(7,'DAVI DE LUCA RODRIGUES','03603603635','41963635241','davi@gmail.com','123456789'),(12,'LUCA RODRIGUES','12312325865','41912451245','lucas@gmail.com','123456');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-06  9:11:53
